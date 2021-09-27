95daa5198def826579139c6151846ba7f9471e50
renan2158 <renan2158@gmail.com>
2021-09-27 18:04:34 -0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/home/styles.css') }}">
+    </head>
+    <body>
+        <div class="homepage-header">
+            <nav>
+                <img alt="Bookseeker logo" src={{ asset('img/logo.svg') }} />
+
+                <div class="nav-links">
+                    <a href="#!">
+                        Contact
+                    </a>
+                    <a href="#!">
+                        About us
+                    </a>
+                </div>
+            </nav>
+        </div>
+        <div class="homepage-container">
+            <div class="search-field">
+                <input placeholder="Search" />
+            </div>
+        </div>
+    </body>
+</html>
